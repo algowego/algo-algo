457849fb07f21c02539b9076532fe57fa71bf12b
DongsuSeo <xl445566@naver.com>
2022-11-21 22:27:32 +0900

    Create 1121숫자짝궁.js

---
+++ algo/dongsu/1121숫자짝궁.js
@@ -0,0 +1,26 @@
+// 프로그래머스
+// level 1
+// https://school.programmers.co.kr/learn/courses/30/lessons/131128#qna
+
+function solution(X, Y) {
+    let answer = '';
+    const number = Array.from(Array(10).fill(0));
+    const twinNumber = [];
+
+    for(let i = 0; i < X.length; i++) {
+        number[X[i]] += 1
+    }
+    
+    for(let j = 0; j < Y.length; j++) {
+        if(number[Y[j]] > 0) {
+            twinNumber.push(Y[j])
+            number[Y[j]] -= 1
+        }
+    }
+
+    answer = twinNumber.length > 0 ? twinNumber.sort((a, b) => b - a).join("") : '-1';
+    
+    if(answer === '') answer = "-1"
+    else if(Number(answer) === 0) answer = "0";
+    return answer;
+}
